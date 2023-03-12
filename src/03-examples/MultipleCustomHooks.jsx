f63418e00852d78fb374f6d0c4ea45200e2c3d7b
redgrin <edgar.elps@gmail.com>
2023-03-11 20:34:38 -0600

    edit css cat facts app

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -3,6 +3,7 @@ import { useCounter } from '../hooks/useCounter';
 import { useAxios } from '../hooks/useAxios'
 
 export const MultipleCustomHooks = () => {
+
     const { counter, increment, decrement } = useCounter( 1, 1 );
     const { data, loading } = useAxios( `https://catfact.ninja/facts?page=${ counter }` )
 
@@ -10,7 +11,12 @@ export const MultipleCustomHooks = () => {
         <> 
             <h1>Fun cats</h1>
             <hr />
-            { loading === false ? ( data.data.map( fact => ( <ul><li key="">{fact.fact}</li></ul> ) )) : ( <p>Loading...</p> )}
+            <ol>
+            { loading === false ? ( 
+                data.data.map( ( fact, index ) => ( 
+                    <li key={ index }>{ fact.fact }</li>
+            ) ) ) : ( <p className='alert alert-info text-center'>Loading...</p> ) }
+            </ol>
             <button className="btn btn-primary" onClick={ decrement }>Previous Page</button> 
             <button className="btn btn-primary" onClick={ increment }>Next Page</button>
         </>
